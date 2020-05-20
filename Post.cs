d1673dd5b1f1fff5e4147177d7c5c395974966b1
Oscar <Oscarponteuk@gmail.com>
2020-05-20 19:35:57 +0100

    Adding Projects to GitHub

---
+++ Post.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace StackOverflow
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        private DateTime _dateCreated;
+        private int _voteValue;
+
+        public Post(string title, string description)
+        {
+            Title = title;
+            Description = description;
+            _dateCreated = DateTime.Now;
+            _voteValue = 0;
+        }
+
+        public void upVote()
+        {
+            _voteValue++;
+        }
+
+        public void downVote()
+        {
+            _voteValue--;
+        }
+
+        public int currentVoteValue()
+        {
+            return _voteValue;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var post = new Post("Testing", "Just testing this program!");
+            post.upVote();
+            post.upVote();
+            post.upVote();
+            post.upVote();
+            post.downVote();
+            Console.WriteLine(post.currentVoteValue());
+
+        }
+    }
+}
